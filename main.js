2c21d05933aaa3a2079a808871be045dbb544ffe
unknown <engr.opriasa@gmail.com>
2021-04-25 15:40:28 +0800

    myProj-Calendar

---
+++ main.js
@@ -0,0 +1,10 @@
+/** @format */
+
+dycalendar.draw({
+  target: "#dycalendar",
+  type: "month",
+  dayformat: "full",
+  monthformat: "full",
+  highlighttargetdate: true,
+  prevnextbutton: "show",
+});
